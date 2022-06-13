9cd82731886e978aa770446b8aa94f30f2972eb0
Sam5196 <sammedpachore1008@gmail.com>
2022-06-13 17:14:26 +0530

    Added:Compare Two Line

--- src/com/bridgelabz/LineComparisonUC3.java
+++ src/com/bridgelabz/LineComparisonUC3.java
@@ -1,12 +1,12 @@
 package com.bridgelabz;
 import java.util.Scanner;
 
-public class LineComparisonUC2 {
-    static void checkEquality() {
+public class LineComparisonUC3 {
+    static void compareLine() {
 
         int x1, x2, y1, y2;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter First Line Input :");
+        System.out.print("Enter First Line :");
         x1 = sc.nextInt();
         x2 = sc.nextInt();
         y1 = sc.nextInt();
@@ -15,25 +15,24 @@ public class LineComparisonUC2 {
         double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         System.out.println("Length of Line1: " + length1);
 
-        System.out.print("Enter Second Line Input :");
+        System.out.print("Enter Second Line :");
         x1 = sc.nextInt();
         x2 = sc.nextInt();
         y1 = sc.nextInt();
         y2 = sc.nextInt();
 
         double length2 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of Line2: " + length2);
+        System.out.println("Length of Line1: " + length2);
 
         if (length1 == length2)
             System.out.println("Line1 and Line2 are equal");
+        else if (length1 > length2)
+            System.out.println("Length of Line1 is greater than Line2");
         else
-            System.out.println("Line1 and Line2 are not equal");
+            System.out.println("Length of Line2 is greater than Line1");
     }
-
 
     public static void main(String[] args) {
-        checkEquality();
+        compareLine();
     }
-}
-
 }
